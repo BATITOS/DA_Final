297198c4c4baa58f91716c7a1e4e979b266515ae
Daniel Kusai <dan.kusai@gmail.com>
2024-11-18 15:21:33 +0200

    code format

--- svm.py
+++ svm.py
@@ -1,10 +1,11 @@
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
 
-def svm(X,y):
+
+def svm(X, y):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
     model = SVC()
     model.fit(X_train, y_train)
     y_pred = model.predict(X_test)
-    
+
     return y_pred
